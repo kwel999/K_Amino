7db91d7cd2793bad105429c0c1ce590e8a2b8d38
Kwel999 <105500624+kwel999@users.noreply.github.com>
2023-03-10 01:03:11 +0600

    Update __init__.py

--- KAmino/__init__.py
+++ KAmino/__init__.py
@@ -6,7 +6,7 @@ from .lib.exception import CheckExceptions
 from .local import Local
 
 version = "1.0.1"
-#newest = get("https://pypi.org/pypi/samino/json").json()["info"]["version"]
+
 
 #if version != newest:
-#    print(f"\033[1;31;33mSAmino New Version!: {newest} (Your Using {version})\033[1;36;33m\nJoin our discord server: \"https://discord.gg/vhBtt2QB\"\033[1;0m")
+#    print(f"\033[1;31;33mNew Version!: {newest} (Your Using {version})\033[1;36;33m\nJoin our discord server: \"https://discord.gg/vhBtt2QB\"\033[1;0m")
