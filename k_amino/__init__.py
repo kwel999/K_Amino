0eea57bb90bd01d048b75288c164a57ce71f46b3
Phoenix <55042406+ThePhoenix78@users.noreply.github.com>
2023-03-11 07:39:45 +1100

    1.0.5

--- k_amino/__init__.py
+++ k_amino/__init__.py
@@ -8,12 +8,12 @@ from .sockets import *
 from .bot import *
 
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 
 __newest__ = loads(get("https://pypi.python.org/pypi/k-amino.py/json").text)["info"]["version"]
 
 if __version__ != __newest__:
-    print(f"New version of {__title__} available: {__newest__} (Using {__version__})")
+    print(f"\033[1;31;38mNew Version!: {__newest__} (Your Using {__version__})\033[1;36;33m\nDiscord server: \"https://discord.gg/zd8gyFJquT\"\033[1;0m")
 else:
-    print(f"version : {__version__}")
+    print(f"\033[1;31;32mversion : {__version__}\033[1;36;33m\nDiscord server: \"https://discord.gg/zd8gyFJquT\"\033[1;0m")
