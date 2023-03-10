fd41c26d86e62261b121e7f2f4f53d8cd4352ebb
Kwel999 <105500624+kwel999@users.noreply.github.com>
2023-03-10 18:14:48 +0600

    Update client.py

--- k_amino/client.py
+++ k_amino/client.py
@@ -605,6 +605,33 @@ class Client(Wss, Session):
         )
         return Json(req)
 
+    def invise_invite(self, chatId: str, userId: Union[str, list]):
+        
+        """
+            Invite users in your room global
+                arguments: 
+                    userId: id object for users
+                    chatId: id object for chats
+                
+                use as:
+                    chat_info = client.get_from_link(input("chat link:  "))
+                    chat_id = chat_info.objectId
+                    # need function for invite user objext
+                    
+      """
+        
+        if isinstance(userId, str): userIds = [userId]
+        elif isinstance(userId, list): userIds = userId
+        else: raise exceptions.WrongType
+
+        data = {
+            "uids": userIds,
+            "timestamp": int(timestamp() * 1000)
+        }
+
+        req = self.postRequest(f"/g/s/chat/thread/{chatId}/member/invite", data)
+        return Json(req) 
+
     def block(self, userId: str):
         req = self.postRequest(f"/g/s/block/{userId}")
         return Json(req)
