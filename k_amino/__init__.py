7cdbba38c423988a3e7295470c5fc0876b0ea414
Kwel999 <105500624+kwel999@users.noreply.github.com>
2023-03-11 19:29:34 +0600

    Update __init__.py

--- k_amino/__init__.py
+++ k_amino/__init__.py
@@ -8,7 +8,7 @@ from .sockets import *
 from .bot import *
 
 
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 
 
 __newest__ = loads(get("https://pypi.python.org/pypi/k-amino.py/json").text)["info"]["version"]
