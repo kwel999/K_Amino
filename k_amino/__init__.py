3a0f2aded07c6181ffd5240586b036c7b53556e4
Kwel999 <105500624+kwel999@users.noreply.github.com>
2023-03-12 17:22:09 +0600

    Update __init__.py

--- k_amino/__init__.py
+++ k_amino/__init__.py
@@ -14,6 +14,6 @@ __version__ = "1.0.7"
 __newest__ = loads(get("https://pypi.python.org/pypi/k-amino.py/json").text)["info"]["version"]
 
 if __version__ != __newest__:
-    print(f"\033[1;31;38mNew Version!: {__newest__} (You are using {__version__})\033[1;36;33m\nDiscord server: \"https://discord.gg/zd8gyFJquT\"\033[1;0m")
+    print(f"\033[1;31;38mk_amino New Version!: {__newest__} (You are using {__version__})\033[1;36;33m\nDiscord server: \"https://discord.gg/zd8gyFJquT\"\033[1;0m")
 else:
-    print(f"\033[1;31;32mversion : {__version__}\033[1;36;33m\nDiscord server: \"https://discord.gg/zd8gyFJquT\"\033[1;0m")
+    print(f"\033[1;31;32mk_amino version : {__version__}\033[1;36;33m\nDiscord server: \"https://discord.gg/zd8gyFJquT\"\033[1;0m")
