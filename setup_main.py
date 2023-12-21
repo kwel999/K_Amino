ee6b4353779d35bfe56ea7ad8a6458ebb00d2eeb
Viktor <99266266+ViktorSky@users.noreply.github.com>
2023-12-21 14:25:13 -0300

    Add files via upload

--- setup_main.py
+++ setup_main.py
@@ -1,9 +1,13 @@
 from urllib.request import urlopen
 from json import loads
 import sys
+import re
 import os
 
 ext = '.tar.gz'
+
+RE_VERSION = re.compile(r'-([0-9.]+)[-]?')
+RE_NAME = re.compile(r'(.*)-[0-9.]+[-]?')
 
 if not os.path.exists('dist'):
     os.system(f'{sys.executable} setup.py sdist bdist_wheel')
@@ -14,10 +18,11 @@ distfiles = sorted(filter(lambda n: n.endswith(ext), os.listdir('dist')), revers
 
 if input('Upload to Pypi? [0,1]: ').strip().lower() in ['1', 'y', 's']:
     command = [sys.executable, '-m', 'twine', 'upload']
-    name, version = distfiles[0].split(ext)[0].rsplit('-', 1)
+    name = RE_NAME.findall(distfiles[0].split(ext)[0])[0]
+    version = RE_VERSION.findall(distfiles[0].split(ext)[0])[0]
     latest = loads(urlopen(f"https://pypi.python.org/pypi/{name}/json").read())["info"]["version"]
     if version.strip() <= latest:
-        if input(f'Override the {version} version? [0,1]: ').strip().lower() in ['1', 'y', 's']:
+        if input(f'Try to overwrite the {version} version? [0,1]: ').strip().lower() in ['1', 'y', 's']:
             command.append('--skip-existing')
     command.append('dist/*')
     os.system(' '.join(command))
